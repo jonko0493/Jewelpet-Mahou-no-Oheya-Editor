e789f1a1328c4fe598897204664865b1a0d99f14
jonko0493 <jonko0493@protonmail.com>
2021-06-22 08:21:45 -0700

    Clean up usings

--- Jewelpet-Mahou-no-Oheya-Editor/GtpcFiles.cs
+++ Jewelpet-Mahou-no-Oheya-Editor/GtpcFiles.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Jewelpet_Mahou_no_Oheya_Editor
 {
---
+++ JewelpetEditorTests/GraphicsFileTests.cs
@@ -0,0 +1,9 @@
+﻿using Jewelpet_Mahou_no_Oheya_Editor;
+using NUnit.Framework;
+using System.IO;
+namespace JewelpetEditorTests
+{
+    public class GraphicsFileTests
+    {
+    }
+}
